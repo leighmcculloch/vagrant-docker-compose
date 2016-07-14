1084200a93ffb9742b282f74818e67eefeae44b8
Leigh McCulloch <leigh@mcchouse.com>
2016-07-13 18:12:44 -0600

    Add `env` configuration option to pass through
    What
    ===
    Add an `env` configuration option, that when set will pass environment
    variables to the docker-compose commands that are run.
    
    Why
    ===
    Fixes #33. Allows setting of configurations in the host and passing them through
    to the docker-compose.yml as environment variables.

--- lib/vagrant-docker-compose/config.rb
+++ lib/vagrant-docker-compose/config.rb
@@ -6,7 +6,7 @@ module VagrantPlugins
         up: "-d"
       }
 
-      attr_accessor :yml, :rebuild, :project_name, :executable_symlink_path, :executable_install_path, :compose_version, :options, :command_options
+      attr_accessor :yml, :rebuild, :project_name, :env, :executable_symlink_path, :executable_install_path, :compose_version, :options, :command_options
 
       def yml=(yml)
         files = yml.is_a?(Array) ? yml : [yml]
@@ -19,6 +19,7 @@ module VagrantPlugins
       def initialize
         @project_name = UNSET_VALUE
         @compose_version = UNSET_VALUE
+        @env = UNSET_VALUE
         @executable_symlink_path = UNSET_VALUE
         @executable_install_path = UNSET_VALUE
         @options = UNSET_VALUE
@@ -28,11 +29,16 @@ module VagrantPlugins
       def finalize!
         @project_name = nil if @project_name == UNSET_VALUE
         @compose_version = "1.6.2" if @compose_version == UNSET_VALUE
+        @env = {} if @env == UNSET_VALUE
         @executable_symlink_path = "/usr/local/bin/docker-compose" if @executable_symlink_path == UNSET_VALUE
         @executable_install_path = "#{@executable_symlink_path}-#{@compose_version}" if @executable_install_path == UNSET_VALUE
         @options = nil if @options == UNSET_VALUE
         @command_options = {} if @command_options == UNSET_VALUE
         @command_options = DEFAULT_COMMAND_OPTIONS.merge(@command_options)
+      end
+
+      def env_s
+        @env.map { |k, v| "#{k}=#{Shellwords.escape(v)}" }.join(" ")
       end
     end
   end
